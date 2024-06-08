510a3941370c8c49c913707010cdee1680836c13
evgeniy-scherbina <evgeniy.shcherbina.es@gmail.com>
2024-06-08 15:00:53 -0400

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM ubuntu:20.04
+
+RUN apt-get update && apt-get install -y wget lsof
+
+RUN wget https://go.dev/dl/go1.22.4.linux-amd64.tar.gz
+
+# Install Golang
+# f: this must be the last flag of the command, and the tar file must be immediately after. It tells tar the name and path of the compressed file.
+# z: tells tar to decompress the archive using gzip
+# x: tar can collect files or extract them. x does the latter.
+# v: makes tar talk a lot. Verbose output shows you all the files being extracted.
+RUN tar -xvzf go1.22.4.linux-amd64.tar.gz -C /usr/local
+# ln -s /path/to/file /path/to/symlink
+RUN ln -s /usr/local/go/bin/go /usr/local/bin/go
+
+# Install Emulator
+WORKDIR /emulator
+COPY . .
+RUN go install .
+# ln -s /path/to/file /path/to/symlink
+RUN ln -s /root/go/bin/emulator /usr/local/bin/emulator
+
+CMD sleep infinity
---
+++ main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"fmt"
+	"github.com/urfave/cli/v2"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"sync"
+	"time"
+)
+
+const (
+	defaultPort = 8080
+
+	defaultDelayInSeconds = 1
+)
+
+func hello(w http.ResponseWriter, req *http.Request) {
+	time.Sleep(defaultDelayInSeconds * time.Second)
+
+	if _, err := fmt.Fprintf(w, "hello\n"); err != nil {
+		fmt.Printf("err: %v\n", err)
+	}
+}
+
+func listenAndServe() {
+	http.HandleFunc("/hello", hello)
+
+	address := fmt.Sprintf("0.0.0.0:%v", defaultPort)
+	fmt.Printf("listening on address: %v\n", address)
+	if err := http.ListenAndServe(address, nil); err != nil {
+		log.Fatalf("can't listen on port %v: %v", defaultPort, err)
+	}
+}
+
+func client(requestsNum int, goroutinesNum int) {
+	var wg sync.WaitGroup
+	wg.Add(goroutinesNum)
+
+	for i := 0; i < goroutinesNum; i++ {
+		go func(i int) {
+			defer wg.Done()
+
+			for j := 0; j < requestsNum; j++ {
+				address := fmt.Sprintf("http://localhost:%v/hello", defaultPort)
+				resp, err := http.Get(address)
+				if err != nil {
+					fmt.Printf("err: %v\n", err)
+					continue
+				}
+
+				body, err := io.ReadAll(resp.Body)
+				if err != nil {
+					fmt.Printf("err: %v\n", err)
+					continue
+				}
+				_ = body
+				//fmt.Printf("body: %s\n", body)
+				//fmt.Printf("goroutine %v, request %v is done\n", i, j)
+			}
+
+			fmt.Printf("goroutine %v is done\n", i)
+		}(i)
+	}
+
+	wg.Wait()
+}
+
+func main() {
+	app := &cli.App{
+		Commands: []*cli.Command{
+			{
+				Name:    "server",
+				Aliases: []string{"s"},
+				Action: func(cCtx *cli.Context) error {
+					listenAndServe()
+					return nil
+				},
+			},
+			{
+				Name:    "client",
+				Aliases: []string{"c"},
+				Action: func(cCtx *cli.Context) error {
+					client(10, 10)
+					return nil
+				},
+			},
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
